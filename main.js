894ad5a59b7064a10bb14d003a701cec0f6e8186
Teri <63741851+artreimagined@users.noreply.github.com>
2020-07-14 14:15:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const testNum = (num) => new Promise ((resolve, reject) => {
   if (num > 50) {
-    resolve('it is greater than fifty')
+    resolve('greater than fifty');
   } else {
-    reject('it is less than fifty')
+    reject('less than fifty');
   }
-})
-testNum(2)
+});
+
+testNum(55)
     .then(function(value) {
-  console.log(value)
+  console.log(value);
 })
 .catch(function(error) {
-  console.log(error)
+  console.log(error);
 })
